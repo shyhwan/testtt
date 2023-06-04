0de946c5ca0dee46e85d93dd3946f0064d12dbd5
shy8818 <83875932+shyhwan@users.noreply.github.com>
2023-06-05 05:49:03 +0900

    프런트 js 만들기

--- app/app.js
+++ app/app.js
@@ -9,8 +9,8 @@ const home = require("./src/routes/home");
 
 // 앱 세팅
 app.set("views", "./src/views");
-// view를 어떤 엔진으로 해석할지 설정 한다.
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // view를 어떤 엔진으로 해석할지 설정 한다.
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use - 미들웨어를 등록해주는 것
 
