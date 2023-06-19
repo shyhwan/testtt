9f5c62dc6b098e7ae413584dda9089fe000033b7
shy8818 <83875932+shyhwan@users.noreply.github.com>
2023-06-19 23:56:58 +0900

    로그 관리 모듈 등록 및 설정

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: process.env.BD_HOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PSWORD,
   database: process.env.DB_DATABASE,
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -8,6 +8,7 @@ class UserStorage {
       const query = "SELECT * FROM users WHERE id = ?;";
       db.query(query, [id], (err, data) => {
         if (err) reject(err);
+        console.log(data);
         resolve(data[0]);
       });
     });
