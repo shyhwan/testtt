660fa9be6536e1ec8f8b5765e61d934973831ec4
shy8818 <83875932+shyhwan@users.noreply.github.com>
2023-06-21 22:24:19 +0900

    winston 프로젝트에 적용

--- app/app.js
+++ app/app.js
@@ -27,7 +27,7 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("tiny", { stream: accessLogStean }));
+app.use(morgan("tiny", { stream: logger.stream }));
 
 app.use("/", home); // use - 미들웨어를 등록해주는 것
 
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -8,6 +8,9 @@ loginBtn.addEventListener("click", login);
 
 function login() {
   if (!id.value) return alert("아이디를 입력해 주십시오.");
+  if (!psword.value) {
+    return alert("비밀번호를 입력해 주십시요.");
+  }
 
   const req = {
     id: id.value,
@@ -26,6 +29,7 @@ function login() {
       if (res.success) {
         location.href = "/";
       } else {
+        if (res.err) return alert(res.err);
         alert(res.msg);
       }
     })
